ef4f4e92d38da404ca25f4e70685593fa8231c6b
Vanshika <vanshika06065@gmail.com>
2022-09-04 12:20:11 +0530

    programs

--- src/Program_compo/Fat_loss.js/Fat_loss.js
+++ src/Program_compo/Fat_loss.js/Fat_loss.js
@@ -5,6 +5,10 @@ import muscleBuil from "../images/muscle-building.png";
 import clinicalCond from "../images/clinical-condition.png";
 import postpartum from "../images/postpartum.png";
 import sportsNutri from "../images/sports-nutrition.png";
+import dedicated from "../images/dedicated_coach.png";
+import doctor from "../images/doctor.png";
+import community from "../images/online-community.png";
+import sustainable from "../images/sustainable_results.png";
 
 const Fat_loss = () => {
   const [fatbutton, setFatbutton] = useState(1);
@@ -17,7 +21,7 @@ const Fat_loss = () => {
             onClick={() => {
               setFatbutton(1);
             }}
-            className={fatbutton==1?`active-step`:`unactive-step`}
+            className={fatbutton == 1 ? `active-step` : `unactive-step`}
           >
             Fat Loss
           </li>
@@ -25,7 +29,7 @@ const Fat_loss = () => {
             onClick={() => {
               setFatbutton(2);
             }}
-            className={fatbutton==2?`active-step`:`unactive-step`}
+            className={fatbutton == 2 ? `active-step` : `unactive-step`}
           >
             Muscle Building
           </li>
@@ -33,7 +37,7 @@ const Fat_loss = () => {
             onClick={() => {
               setFatbutton(3);
             }}
-            className={fatbutton==3?`active-step`:`unactive-step`}
+            className={fatbutton == 3 ? `active-step` : `unactive-step`}
           >
             Postpartum
           </li>
@@ -41,7 +45,7 @@ const Fat_loss = () => {
             onClick={() => {
               setFatbutton(4);
             }}
-            className={fatbutton==4?`active-step`:`unactive-step`}
+            className={fatbutton == 4 ? `active-step` : `unactive-step`}
           >
             Clinical Condition
           </li>
@@ -49,7 +53,7 @@ const Fat_loss = () => {
             onClick={() => {
               setFatbutton(5);
             }}
-            className={fatbutton==5?`active-step`:`unactive-step`}
+            className={fatbutton == 5 ? `active-step` : `unactive-step`}
           >
             Sports Nutrition
           </li>
@@ -73,9 +77,9 @@ const Fat_loss = () => {
           <img className="fat-loss-img" src={muscleBuil} />
 
           <p>
-            Sustainably achieve and maintain healthy body weight with our weight
-            loss program created by expert coaches to develop long-term healthy
-            eating habits to improve your lifestyle with our support.
+            Suitable for both beginners and Build mass and sculpt your physique
+            to the next level with our muscle-building fitness and nutrition
+            programme.
           </p>
         </div>
       )}
@@ -85,9 +89,9 @@ const Fat_loss = () => {
           <img className="fat-loss-img" src={postpartum} />
 
           <p>
-            Sustainably achieve and maintain healthy body weight with our weight
-            loss program created by expert coaches to develop long-term healthy
-            eating habits to improve your lifestyle with our support.
+            Secure a smooth and healthy journey of nourishment for yourself and
+            your baby through all your three trimesters by following our
+            clinical nutrition plans.
           </p>
         </div>
       )}
@@ -97,24 +101,65 @@ const Fat_loss = () => {
           <img className="fat-loss-img" src={clinicalCond} />
 
           <p>
-            Sustainably achieve and maintain healthy body weight with our weight
-            loss program created by expert coaches to develop long-term healthy
-            eating habits to improve your lifestyle with our support.
+            Combat lifestyle diseases such as PCOS/PCOD, Renal Disorders,
+            Thyroid, Hypertension, Diabetes, Gastrointestinal and Geriatric
+            disorders with clinical nutrition and fitness plans.
           </p>
         </div>
       )}
-
       {fatbutton == 5 && (
         <div className="fat-loss-info">
           <img className="fat-loss-img" src={sportsNutri} />
 
           <p>
-            Sustainably achieve and maintain healthy body weight with our weight
-            loss program created by expert coaches to develop long-term healthy
-            eating habits to improve your lifestyle with our support.
+            Combat lifestyle diseases such as PCOS/PCOD, Renal Disorders,
+            Thyroid, Hypertension, Diabetes, Gastrointestinal and Geriatric
+            disorders with clinical nutrition and fitness plans.
           </p>
         </div>
       )}
+
+      <div className="three-methodology under-fat">
+        <div>
+          <img src={dedicated} />
+          <h3>Dedicated Coach</h3>
+          <p>
+            A health coach shall create a custom plan for you based on their
+            expertise and you requirement. They shall be accessible throughout
+            your fitness and wellness journey and offer you motivation if you
+            ever lose sight of your vision.
+          </p>
+        </div>
+        <div>
+          <img src={sustainable} />
+          <h3>Sustainable Results</h3>
+          <p>
+            Our plans shall ensure that you’re empowered with the information
+            and know-how to maintain your results even after your programme has
+            ended.
+          </p>
+        </div>
+        <div>
+          <img src={community} />
+          <h3>Online Community</h3>
+          <p>
+            Be a part of a global community of health-enthusiasts of 20000+
+            individuals working towards the shared common goal of building a
+            world that’s fitter, healthier and happier!
+          </p>
+        </div>
+        <div>
+          <img src={doctor} />
+          <h3>Board of Doctors</h3>
+          <p>
+            Our coaching methodologies and strategies are approved by medical
+            health professionals and research scholars who associate with us to
+            work on clinical cases such Diabetes, PCOS, Thyroid, Intolerances,
+             Gastrointestinal Disorders etc.
+          </p>
+        </div>
+      </div>
+      
     </div>
   );
 };
--- src/pages/Programs_page.js
+++ src/pages/Programs_page.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Carouseel from '../components/carousel/Carouseel';
+import Footer from '../components/footer/Footer';
 import Navbar from '../components/Navbar/Navbar';
 import Chooseprogram from '../Program_compo/chooseProgram/Chooseprogram';
 import Fat_loss from '../Program_compo/Fat_loss.js/Fat_loss';
@@ -9,6 +11,9 @@ const Programs_page = () => {
       <Navbar/>
       <Chooseprogram/>
       <Fat_loss/>
+      <Carouseel/>
+      <div className='start-your-health'>Start your health journey with us from today!</div>
+      <Footer/>
     </div>
   )
 }
