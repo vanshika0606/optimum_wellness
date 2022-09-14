399499e9e2041b75afef8c24f398fb1c4777976c
Vanshika <vanshika06065@gmail.com>
2022-09-14 19:33:30 +0530

    about page

---
+++ src/About_compo/About_story/Ready.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './about_story.css'
+
+const Ready = () => {
+  return (
+    <div className='ready'>
+      <h3 style={{fontSize:'3vw', color:'#7621EC'}}>Ready To Takecare Of Your Wellness ?</h3>
+      <button >Contact us </button>
+    </div>
+  )
+}
+
+export default Ready
--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,11 @@ import Coaches from '../components/Coaches/Coaches'
 import Two_coaches from '../About_compo/About_story/Two_coaches'
 import square from '../About_compo/images/doctors_square_img.png'
 import squaree from '../components/images/square-design.png'
+import mark from '../About_compo/images/question_mark.png'
+import Ready from '../About_compo/About_story/Ready.js';
+import Footer from '../components/footer/Footer'
+
+
 const About = () => {
   return (
     <div>
@@ -13,6 +18,10 @@ const About = () => {
       <Two_coaches/>
       <Coaches heading={"MEET OUR COACHES"} headingColor={'rgb(118, 33, 236)'} square={squaree} background={'white'} nameColor={'rgb(118, 33, 236)'} headingPaddingTop={'0vw'} bottomImages={'-7vw'}  professionColor={'black'}/>
       <Coaches heading={"BOARD OF DOCTORS"} headingColor={'white'} square={square} background={'rgb(118, 33, 236)'} nameColor={'white'} headingPaddingTop={'4vw'} bottomImages={'-4vw'}  professionColor={'white'}/>
+
+      <img src={mark} style={{width:'100vw'}} />
+      <Ready/>
+      <Footer/>
     </div>
   )
 }
