067f9bd586bdd850c23b6559cf6e1a28a7402739
Vanshika <vanshika06065@gmail.com>
2022-09-14 19:49:26 +0530

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,8 @@ import square from '../About_compo/images/doctors_square_img.png'
 import squaree from '../components/images/square-design.png'
 import mark from '../About_compo/images/question_mark.png'
 import Ready from '../About_compo/About_story/Ready.js';
-import Footer from '../components/footer/Footer'
+import Footer from '../components/footer/Footer';
+import '../About_compo/About_story/about_story.css'
 
 
 const About = () => {
@@ -18,9 +19,10 @@ const About = () => {
       <Two_coaches/>
       <Coaches heading={"MEET OUR COACHES"} headingColor={'rgb(118, 33, 236)'} square={squaree} background={'white'} nameColor={'rgb(118, 33, 236)'} headingPaddingTop={'0vw'} bottomImages={'-7vw'}  professionColor={'black'}/>
       <Coaches heading={"BOARD OF DOCTORS"} headingColor={'white'} square={square} background={'rgb(118, 33, 236)'} nameColor={'white'} headingPaddingTop={'4vw'} bottomImages={'-4vw'}  professionColor={'white'}/>
-
+      <div className='question-ready'>
       <img src={mark} style={{width:'100vw'}} />
       <Ready/>
+      </div>
       <Footer/>
     </div>
   )
